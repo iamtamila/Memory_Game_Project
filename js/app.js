845b2a5619ea324a75b9f0171a4dda1349cbf553
Tamila <itstamila@gmail.com>
2018-06-14 12:28:57 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -12,12 +12,6 @@ var cards = ['fa-diamond', 'fa-diamond',
 function generateCard(card) {
   return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -31,20 +25,6 @@ function shuffle(array) {
     }
     return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
 
 /*GAMEPLAY*/
 function myGame(){
@@ -104,9 +84,12 @@ function moveCount() {
   if (moves == 1) {
     startTimer();
     }
-  if (moves >= 14) {
+  if (moves >= 16 && moves < 20) {
     stars.removeChild(stars.childNodes[1]);
-  }
+    }
+    if (moves>100) {
+      stars.removeChild(stars.childNodes[1]);
+      }
 }
 
 /*TIMER*/
@@ -146,54 +129,43 @@ function resetTimer (){
 /*RESTART MYGAME*/
 const restart = document.querySelector('.restart');
 restart.addEventListener('click',function(){
-  moves = 0;
-  moveCounter.textContent = '0';
-  stopTimer ();
-  resetTimer ();
-  openCards = [];
-  //ADD RESTART FUNCTION
-
-  myGame();
+location.reload();
 });
 
-
 /*GAME END*/
-
-//When all cards match, change modal css so that modal is shown
+//declaring let
 let matchedCards = document.getElementsByClassName('match');
 let modal = document.querySelector('.modal');
 let finalTime = document.querySelector('.finalTime');
 let finalRating = document.querySelector('.finalRating');
 let finalMoves = document.querySelector('.finalMoves');
 
+// terms of win
 function winGame() {
   if (matchedCards.length === 16) {
     modal.style.display = "block";
     finalRating.innerHTML = stars.innerHTML;
     finalMoves.innerHTML = moveCounter.innerHTML;
     finalTime.innerHTML = timer.innerHTML;
+    stopTimer ();
   }
 }
-// Get the <span> element that closes the modal
+// <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 };
-// When the user clicks anywhere outside of the modal, close it
+//  click anywhere outside of the modal - close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 };
-//Play again button will clear modal and reset timer
+// Play again button will reset game
 document.querySelector('.button').addEventListener('click', playAgain);
-document.querySelector('.button').addEventListener('click', resetTimer);
 document.querySelector('.restart').addEventListener('click', playAgain);
 
 function playAgain() {
-  modal.style.display = "none";
-  moveCounter.innerHTML = 0;
-  one.style.visibility = 'visible';
-  two.style.visibility = 'visible';
+  location.reload();
 }
